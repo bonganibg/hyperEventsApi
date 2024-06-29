2e4f600a6f85b0fc8c157c0b07bad730c2fd6b45
Bonganibg <bongani.gumbo@yahoo.com>
2024-06-29 22:38:54 +0200

    tour routes are good

--- src/controllers/tour.controller.js
+++ src/controllers/tour.controller.js
@@ -1,5 +1,5 @@
 const TourModel = require("../models/tour.model");
-const { createTour } = require("../services/tour.service")
+const { createTour, updateTour, deleteTour, getTour, getTours } = require("../services/tour.service")
 
 exports.createTour = async (req, res) => {
     const result = await createTour(req.body);
@@ -19,11 +19,68 @@ exports.createTour = async (req, res) => {
 
 exports.getTour = async (req, res) => {
     const id = req.params.id;
+    const result = await getTour(id);
 
-    try{
-        const tour = await TourModel.findById(id)
-        res.status(200).json(tour)
-    } catch(err){
-        res.status(400).json(err.message)
+    if (result.success){
+        res.status(200).json({
+            message: "Fetched tour",
+            tour: result.tour
+        })
+    } else{
+        res.status(400).json({
+            message: "Failed to fetch tour",
+            error: result.error
+        })    
+    }
+}
+
+exports.getTours = async (req, res) => {
+    const result = await getTours();
+
+    if (result.success){
+        res.status(200).json({
+            message: "Fetched tours",
+            tours: result.tours
+        })
+    } else{
+        res.status(400).json({
+            message: "Failed to fetch tours",
+            error: result.error
+        })
+    }
+}
+
+exports.updateTour = async (req, res) => {
+    const tourDetails = req.body;
+    tourDetails.id = req.params.id;
+    const result = await updateTour(tourDetails);
+
+    if (result.success){
+        res.status(200).json({
+            message: "Updated tour",
+            id: result.id
+        })
+    } else{
+        res.status(400).json({
+            message: "Failed to update tour",
+            error: result.error
+        })
+    }
+}
+
+exports.deleteTour = async (req, res) => {
+    const id = req.params.id;
+    const result = await deleteTour(id);
+
+    if (result.success){
+        res.status(200).json({
+            message: "Deleted tour",
+            id: result.id
+        })
+    } else{
+        res.status(400).json({
+            message: "Failed to delete tour",
+            error: result.error
+        })
     }
 }
--- src/routes/tour.route.js
+++ src/routes/tour.route.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
-const { createTour, getTour } = require("../controllers/tour.controller");
+const { createTour, getTour, getTours, updateTour, deleteTour } = require("../controllers/tour.controller");
  
-router.post('/', createTour)
-router.get('/:id', getTour)
+router.post('/', createTour);
+router.get('/:id', getTour);
+router.get("/", getTours);
+router.put("/:id", updateTour);
+router.delete("/:id", deleteTour);
 
 module.exports = router;
